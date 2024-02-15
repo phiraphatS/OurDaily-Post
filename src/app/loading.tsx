02723e15907345b99efaae7c47520e86d4186612
Phiraphat-Codediva <phiraphat@codediva.co.th>
2024-02-15 18:45:52 +0700

    update

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function loading() {
+  return (
+    <div className="loader">
+        <div className="justify-content-center jimu-primary-loading"></div>
+    </div>
+  )
+}
